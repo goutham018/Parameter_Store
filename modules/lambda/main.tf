3b7773acfaea8ad62711c6270ca6aa1d0f857590
goutham018 <gouthamr522@gmail.com.com>
2025-05-16 12:47:50 +0530

    updated messages

---
+++ modules/lambda/main.tf
@@ -0,0 +1,50 @@
+
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda-ssm-access-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        },
+        Effect = "Allow",
+        Sid    = ""
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "lambda_ssm_policy" {
+  name        = "lambda-ssm-read"
+  description = "Policy to allow Lambda to read from Parameter Store"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "ssm:GetParameter",
+          "ssm:GetParameters",
+          "ssm:GetParameterHistory"
+        ],
+        Effect   = "Allow",
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = aws_iam_policy.lambda_ssm_policy.arn
+}
+
+resource "aws_lambda_function" "read_ssm" {
+  filename         = "lambda_function_payload.zip"
+  function_name    = "readSSMParameterFunction"
+  role             = aws_iam_role.lambda_exec_role.arn
+  handler          = "lambda_function.lambda_handler"
+  runtime          = "python3.9"
+  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+}
