8f1e5da75372ae9b65ac13519d11e843878eee8d
goutham018 <66243642+goutham018@users.noreply.github.com>
2025-05-16 13:57:44 +0530

    Updated main.tf

--- modules/lambda/main.tf
+++ modules/lambda/main.tf
@@ -46,5 +46,5 @@ resource "aws_lambda_function" "read_ssm" {
   role             = aws_iam_role.lambda_exec_role.arn
   handler          = "lambda_function.lambda_handler"
   runtime          = "python3.9"
-  source_code_hash = filebase64sha256("lambda_function_payload.zip")
+  source_code_hash = filebase64sha256("./lambda_function_payload.zip")
 }
