4c23183d44a6d8140ba15bcd486963433ebfe22f
goutham018 <66243642+goutham018@users.noreply.github.com>
2025-05-16 14:05:48 +0530

    Updated main.tf

--- modules/kms/main.tf
+++ modules/kms/main.tf
@@ -1,9 +1,39 @@
-
 resource "aws_kms_key" "parameter_encryption" {
-  description             = "KMS key for encrypting parameter store values"
+  description             = "KMS key for encrypting SSM parameters"
   deletion_window_in_days = 7
-  enable_key_rotation     = true
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "Allow Lambda Role to decrypt",
+        Effect    = "Allow",
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/lambda-ssm-access-role"
+        },
+        Action    = ["kms:Decrypt"],
+        Resource  = "*"
+      },
+      {
+        Sid       = "Enable IAM User Permissions",
+        Effect    = "Allow",
+        Principal = {
+          AWS = "*"
+        },
+        Action    = "kms:*",
+        Resource  = "*",
+        Condition = {
+          StringEquals = {
+            "kms:CallerAccount" = "${data.aws_caller_identity.current.account_id}"
+          }
+        }
+      }
+    ]
+  })
 }
+
+data "aws_caller_identity" "current" {}
+
 
 output "kms_key_id" {
   value = aws_kms_key.parameter_encryption.key_id
